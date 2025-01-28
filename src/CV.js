f8bb2006b8feaf4e5bc408505da5397f4869f17d
upavloff <pavloffulysse@gmail.com>
2025-01-28 18:35:45 +0100

    cv date update

--- src/CV.js
+++ src/CV.js
@@ -22,7 +22,7 @@ function CV() {
                     {/* Link to CV */}
                     <a href="/Pavloff_CV.pdf" target="_blank" rel="noopener noreferrer" className="cv-link">
                         View my full CV [PDF]
-                    </a>      (updated 01/12/2024).
+                    </a>      (updated 10/01/2025).
                 </div>
                 {/* Profile image */}
                 <div className="cv-image">
